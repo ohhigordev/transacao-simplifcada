e983d36174d9b4e91903da198de7543075122642
ohhigordev <160289988+ohhigordev@users.noreply.github.com>
2026-03-23 19:50:29 -0300

    chore: initial project structure from spring initializr

---
+++ src/main/java/com/ohhigordev/transacao_simplificada/TransacaoSimplificadaApplication.java
@@ -0,0 +1,13 @@
+package com.ohhigordev.transacao_simplificada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransacaoSimplificadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransacaoSimplificadaApplication.class, args);
+	}
+
+}
